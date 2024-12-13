659e35338e3e6d185f16bda9b54f10846e01c772
tarorg <tarframework@gmail.com>
2024-12-13 07:46:40 -0800

    t1.1

---
+++ src/lib/components/sidebar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import AppSidebar from "$lib/components/app-sidebar.svelte";
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+</script>
+
+<Sidebar.Provider>
+    <AppSidebar />
+    <Sidebar.Inset>
+        <header class="flex h-16 shrink-0 items-center gap-2">
+            <div class="flex items-center gap-2 px-4">
+                <Sidebar.Trigger />
+            </div>
+        </header>
+        <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <slot />
+        </div>
+    </Sidebar.Inset>
+</Sidebar.Provider>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Sidebar from "$lib/components/sidebar.svelte";
+</script>
+
+<div class="flex">
+    <Sidebar>
+        <main class="flex-1">
+        </main>
+    </Sidebar>
+</div>
