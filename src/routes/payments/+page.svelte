afb782a917cb0b28a461fbb8fc5dfa9d1b5d4b2d
tarorg <tarframework@gmail.com>
2024-12-15 00:47:02 -0800

    t1.2.8

---
+++ src/routes/payments/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Dashboard from "$lib/components/dashboard-06.svelte";
+	import Sidebar from "$lib/components/sidebar.svelte";
+</script>
+
+<div class="flex">
+	<Sidebar />
+	<div class="flex-1">
+		<Dashboard />
+	</div>
+</div>
